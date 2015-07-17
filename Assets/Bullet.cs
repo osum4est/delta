bca5a7ebb26bbd3f63e603d6f44a0e767d187c82
Forrest <forrestjones@8bitforest.com>
2015-07-17 12:52:42 -0600

    Added bullet prefab

---
+++ Assets/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public float force = 15f;
+
+	void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0, force));
+    }
+}

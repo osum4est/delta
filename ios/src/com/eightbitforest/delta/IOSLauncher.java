3d44458642b570370397e0cc76309c4154b513c9
Forrest Jones <fjones-dev@8bitforest.com>
2017-09-20 13:29:16 -0600

    Update libgdx, html working

--- ios/src/com/eightbitforest/delta/IOSLauncher.java
+++ ios/src/com/eightbitforest/delta/IOSLauncher.java
@@ -1,32 +1,18 @@
 package com.eightbitforest.delta;
 
-import com.eightbitforest.delta.googleplay.ViewController;
-import org.robovm.apple.coregraphics.CGRect;
-import org.robovm.apple.foundation.NSArray;
-import org.robovm.apple.foundation.NSAutoreleasePool;
-import org.robovm.apple.foundation.NSDictionary;
-import org.robovm.apple.foundation.NSString;
-import org.robovm.apple.uikit.*;
-
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.eightbitforest.delta.DeltaMain;
-import org.robovm.pods.google.games.GPGManager;
-import org.robovm.pods.google.games.GPGToastPlacement;
-
-import java.util.ArrayList;
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
-
-
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new DeltaMain(new iOSGooglePlay(this)), config);
+        return new IOSApplication(new DeltaMain(), config);
     }
 
     public static void main(String[] argv) {
-
         NSAutoreleasePool pool = new NSAutoreleasePool();
         UIApplication.main(argv, null, IOSLauncher.class);
         pool.close();
